3efa83f8ef33d47dad17a33de6045f6eb0ff5c2d
Dylan <dj0759@gmail.com>
2017-10-14 20:04:18 -0700

    updates with dashboard + navbar

--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -43,7 +43,33 @@ class Dashboard extends Component {
   //       //  //this.setState({itins_retrieved : false});        
   //     })
   // }
+  renderLinks () {
+    if (this.props.authenticated) {
+      // show a link for user to go to Dashboard or Sign Out
+      return [
 
+        <li className='nav-item active' key={2}>
+          <Link className='btn btn-default tg-login__btn' to='/dashboard'>Dashboard</Link>
+        </li>
+        ,
+        <li className='nav-item' key={3}>
+          <Link className='nav-link' to='/signout'>Sign Out</Link>
+        </li>
+      ];
+    } else {
+      // show a link for user to Sign In or Sign Up
+      return [
+        <li className='nav-item active' key={2}>
+          <Link className='btn btn-default tg-login__btn' to='/dashboard'>Dashboard</Link>
+        </li>
+        ,
+        <li className='nav-item' key={3}>
+          <Link className='nav-link' to='/signout'>Sign Out</Link>
+        </li>
+    
+      ];
+    }
+  };
   render() {
     //Grabs the users email from localStorage
       // localStorage.setItem("USER_EMAIL", email) -- to set a variable email to the local storage variable of USER_EMAIL
@@ -52,26 +78,28 @@ class Dashboard extends Component {
   return (      
       <div className='dashboard'>
         <nav className="navbar navbar-default">
-          <div className="container-fluid">
-            <div className="navbar-header">
-              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span className="sr-only">Toggle navigation</span>
-                <span className="icon-bar"></span>
-              </button>
-              <a className="navbar-brand" href="#">
-              <strong>App Name Here</strong></a>
-            </div>
-            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-              <ul className="nav navbar-nav">
-                <li className="active"><a href="/">Home <span className="sr-only">(current)</span></a></li>
-                <li><a href="#">Tab 2</a></li>
-                <li><a href="#">Tab 3</a></li>
-
-              </ul>
-            
-            </div>
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar"></span>
+            </button>
+            <a className="navbar-brand" href="#">
+            <strong>App Name Here</strong></a>
           </div>
-        </nav>
+          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul className="nav navbar-nav">
+              <li><a href="/">Home </a></li>
+              <li><a href="#">Tab 2</a></li>
+              <li><a href="#">Tab 3</a></li>
+
+            </ul>
+            <ul className="nav navbar-nav navbar-right">
+              {this.renderLinks()}
+            </ul>
+          </div>
+        </div>
+      </nav>
         <h3>Dashboard</h3>
         <div className='col-md-6'>
           {/* <h4><strong>Your Previously saved Itineraries</strong></h4>
@@ -86,7 +114,7 @@ class Dashboard extends Component {
 
           <h4>Welcome to the dashboard, you have logged in correctly :) </h4>
           {
-            Email ? `<p>Your email is, ${Email}` : <p>Something went wrong I think </p>
+            Email ? `<p>Your email is, ${Email}` : <p>Something went wrong getting your email </p>
           }
           
         </div>
